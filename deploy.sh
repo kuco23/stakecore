99bedcb3bf701c4f152a753d306be2d17ec10345
kuco23 <nseverkar@gmail.com>
2025-02-06 13:02:03 +0100

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
 git commit -am "Save uncommited changes (WIP)"
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f dist
 git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout $branch
